0ff2539816f562cc080a27d1c38d514df8ed48e2
manuelbre <bmanuel@ethz.ch>
2018-11-01 14:08:53 +0100

    Finished ex5.

--- assignments/ex5/lbfgsb.py
+++ assignments/ex5/lbfgsb.py
@@ -2,15 +2,15 @@ import scipy.optimize as spo
 import numpy as np
 import torch
 
-def lbfgsb(var, min_val, max_val, loss_fn, zero_grad_fn):
+def lbfgsb(var, min_val, max_val, loss_fn, zero_grad_fn, **kwargs):
     x = var.detach().numpy()
     shape = x.shape
     x = x.ravel().astype(np.float64) # l-bfgs-b uses double apparently
     bounds = [(min_val, max_val)] * x.shape[0]
     def f(x):
         var.detach()[:] = torch.from_numpy(x.reshape(shape))
-        loss = loss_fn()
-        zero_grad_fn()
+        loss = loss_fn(**kwargs)
+        zero_grad_fn(**kwargs)
         loss.backward()
         f = loss.detach().numpy().astype(np.float64)
         g = var.grad.detach().numpy().ravel().astype(np.float64)
